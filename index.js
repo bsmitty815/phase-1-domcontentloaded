c2a85f42dfcbfaa2ec4682b79deee8556c1ea920
Brian Smith <brian.bubbam@gmail.com>
2021-05-24 09:50:29 -0500

    commit tree be37064bd8cb34b9cb80399846cbba76003fdeb1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sentance = document.getElementById('text');
+    sentance.textContent=`This is really cool!`;
+
+
+
+});
